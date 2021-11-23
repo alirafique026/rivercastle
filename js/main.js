8c70c44496ad2b57b58d946a6c83a820b4eed4c7
ali <ali.rafique.026@gmail.com>
2021-11-23 11:16:59 +0530

    slider added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.cent').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    infinite: true,
+    arrows: false,
+});
+
+})
